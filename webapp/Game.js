b26c8ed6984fed859cd13c03ec820365a37f6b6a
Christian Grail <christian.grail@sap.com>
2018-04-23 22:34:09 +0000

    Add particle explosion

--- webapp/Game.js
+++ webapp/Game.js
@@ -67,6 +67,7 @@ class Game {
 			var laserBox = new THREE.Box3().setFromObject(laser);
 			if (this.tieFighterBox.intersectsBox(laserBox)) {
 				scene.remove(laser);
+				this.explode();
 				this.spawnFighter();
 				tween.stop();
 			}
@@ -85,7 +86,7 @@ class Game {
 		if (this.boxHelper) {
 			this.boxHelper.update();
 		}
-		if(this.tieFighter) {
+		if (this.tieFighter) {
 			this.tieFighterBox = new THREE.Box3().setFromObject(this.tieFighter);
 		}
 	}
@@ -115,6 +116,57 @@ class Game {
 			this.vrFrameData.pose.orientation[2],
 			this.vrFrameData.pose.orientation[3]
 		);
+	}
+
+	explode() {
+
+		var material = new THREE.SpriteMaterial({
+			map: new THREE.CanvasTexture(this.generateSprite()),
+			blending: THREE.AdditiveBlending
+		});
+		for (var i = 0; i < 100; i++) {
+			var particle = new THREE.Sprite(material);
+			this.initParticle(particle, i * 10);
+			this.scene.add(particle);
+		}
+	}
+
+	generateSprite() {
+		var canvas = document.createElement('canvas');
+		canvas.width = 16;
+		canvas.height = 16;
+		var context = canvas.getContext('2d');
+		var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width /
+			2);
+		gradient.addColorStop(0, 'rgba(255,255,255,1)');
+		gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
+		gradient.addColorStop(0.4, 'rgba(0,0,64,1)');
+		gradient.addColorStop(1, 'rgba(0,0,0,1)');
+		context.fillStyle = gradient;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		return canvas;
+	}
+
+	initParticle(particle, delay) {
+		particle.position.set(0, 0, 0);
+		particle.scale.x = particle.scale.y = Math.random() * 32 + 16;
+		var duration = 1000;
+		new TWEEN.Tween(particle)
+			.to({}, duration)
+			.start();
+		new TWEEN.Tween(particle.position)
+			.to({
+				x: Math.random() * 4000 - 2000,
+				y: Math.random() * 1000 - 500,
+				z: Math.random() * 4000 - 2000
+			}, duration)
+			.start();
+		new TWEEN.Tween(particle.scale)
+			.to({
+				x: 0.01,
+				y: 0.01
+			}, duration)
+			.start();
 	}
 
 }
