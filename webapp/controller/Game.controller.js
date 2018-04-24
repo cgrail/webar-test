37410653d810a99ee69d48409b6c4a174880aeaa
Christian Grail <christian.grail@sap.com>
2018-04-24 21:51:18 +0000

    Load Tie fighter

--- webapp/controller/Game.controller.js
+++ webapp/controller/Game.controller.js
@@ -4,18 +4,45 @@ sap.ui.define([
 ], function(Controller, MessageToast) {
 	"use strict";
 
-	return Controller.extend("webar-test.controller.App", {
+	return Controller.extend("webar-test.controller.Game", {
 		onInit: function() {
 			this.arView = this.byId("arView");
+			this.loadTieFighter();
 		},
 
-		onShowHello: function() {
-			this.shoot();
-			MessageToast.show("Fire");
+		loadTieFighter: function() {
+			new THREE.MTLLoader()
+				.load('3dmodel/materials.mtl', function(materials) {
+					materials.preload();
+					new THREE.OBJLoader()
+						.setMaterials(materials)
+						.load('3dmodel/tie.obj', function(object) {
+							var scale = 0.1
+							object.scale.set(scale, scale, scale);
+							this.tieFighter = object;
+							this.spawnFighter();
+						}.bind(this));
+				}.bind(this));
+		},
+
+		spawnFighter: function() {
+			var fighter = this.tieFighter;
+			var target = this.getPositionWithOffset(1);
+			target.x -= Math.random();
+			var initialPos = target.clone();
+			initialPos.z -= 10;
+			fighter.position.copy(initialPos);
+			fighter.quaternion.copy(this.getOrientation());
+			this.getScene().add(fighter);
+			var tween = new TWEEN.Tween(initialPos).to(target, 2000);
+			tween.onUpdate(function() {
+				fighter.position.z = initialPos.z;
+			});
+			tween.start();
 		},
 
 		shoot: function() {
-			var scene = this.arView.getThreeContext().scene;
+			var scene = this.getScene();
 			var geometry = new THREE.BoxGeometry(0.03, 0.03, 2);
 			var material = new THREE.MeshBasicMaterial({
 				color: 0xff0000
@@ -34,11 +61,15 @@ sap.ui.define([
 			});
 			tween.onComplete(function() {
 				scene.remove(laser);
-			}.bind(this));
+			});
 			tween.start();
 			scene.add(laser);
 			var laserFire = new Audio("sound/Laser.mp3");
 			laserFire.play();
+		},
+
+		getScene: function() {
+			return this.arView.getThreeContext().scene;
 		},
 
 		getPositionWithOffset: function(offset) {
